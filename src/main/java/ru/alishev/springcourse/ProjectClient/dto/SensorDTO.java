1f19aac4088e4b70f27b1bbe697966235aadb44d
jana <jantschick3@gmail.com>
2023-02-07 16:04:38 +0300

    Initial commit

---
+++ src/main/java/ru/alishev/springcourse/ProjectClient/dto/SensorDTO.java
@@ -0,0 +1,13 @@
+package ru.alishev.springcourse.ProjectClient.dto;
+
+public class SensorDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/ru/alishev/springcourse/ProjectClient/ProjectClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alishev.springcourse.ProjectClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
